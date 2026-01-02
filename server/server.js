65daced9bfafed15e1c043489de30f86fdc22f63
divya-murthy26 <divyadn2005@gmail.com>
2026-01-02 17:03:16 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Parse JSON bodies // middlware
 app.use(cors({
   origin: ['http://localhost:5173',
-  'https://airesumebuilder-frontend.onrender.com'],
+  'https://airesumebuilder-client.onrender.com'],
   credentials: true
 }));         // Enable Cross-Origin Resource Sharing
 
